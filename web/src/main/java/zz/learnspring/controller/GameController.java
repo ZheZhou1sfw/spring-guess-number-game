b41385958f52ee02b9c71df9e2349182fc5fb002
Zhe <zhezhou@gatech.edu>
2020-08-02 15:42:45 -0400

    added play and redirect templates and controller logic

---
+++ web/src/main/java/zz/learnspring/controller/GameController.java
@@ -0,0 +1,50 @@
+package zz.learnspring.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import zz.learnspring.service.GameService;
+import zz.learnspring.util.AttributeNames;
+import zz.learnspring.util.GameMappings;
+import zz.learnspring.util.ViewNames;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class GameController {
+
+    // == fields ==
+    private final GameService gameService;
+
+    // == requrest methods ==
+    @GetMapping(GameMappings.PLAY)
+    public String play(Model model) {
+        model.addAttribute(AttributeNames.MAIN_MESSAGE, gameService.getMainMessage());
+        model.addAttribute(AttributeNames.RESULT_MESSAGE, gameService.getResultMesage());
+        log.info("model = {}", model);
+
+        if (gameService.isGameOver()) {
+            return ViewNames.GAME_OVER;
+        }
+
+        return ViewNames.PLAY;
+    }
+
+    @PostMapping(GameMappings.PLAY)
+    public String processMessage(@RequestParam int guess) {
+        log.info("guess = {}", guess);
+        gameService.checkGuess(guess);
+        return GameMappings.REDIRECT_PLAY;
+    }
+
+    @GetMapping(GameMappings.RESTART)
+    public String restart() {
+        gameService.reset();
+        return GameMappings.REDIRECT_PLAY;
+    }
+
+}
